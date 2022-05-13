a7bd5791fed66e33155b503a9ba9c7ab1ca09374
ivdvorkovoy <Y6cA4V9maGAVeyQ>
2022-05-13 23:11:30 +0300

    Подключен bootstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,18 +6,41 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
 </head>
 <body>
-<div>
-    <ul>
-        <li><a href="{{ route('main.index') }}">Главная</a></li>
-        <li><a href="{{ route('post.index') }}">Публикации</a></li>
-        <li><a href="{{ route('about.index') }}">О проекте</a></li>
-        <li><a href="{{ route('contact.index') }}">Контакты</a></li>
-    </ul>
+<div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">LaraLearn</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('main.index') }}">Главная</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('post.index') }}">Публикации</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('about.index') }}">О проекте</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('contact.index') }}">Контакты</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </div>
-<div>
-    @yield('content')
+
+<div class="container">
+    <div class="container-fluid">
+        @yield('content')
+    </div>
 </div>
 
 </body>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,4 +1,25 @@
 @extends('layouts.main')
 @section('content')
-    <div>Страница с публикациями</div>
+    <h3>Страница с публикациями</h3>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Заголовок</th>
+            <th scope="col">Контент</th>
+            <th scope="col">Лайки</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($posts as $post)
+            <tr>
+                <th scope="row">{{ $post->id }}</th>
+                <td>{{ $post->title }}</td>
+                <td>{{ $post->content }}</td>
+                <td>{{ $post->likes }}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+
 @endsection
