ab8d7fd90c5f6effef93e56a7786000bd4fac67f
ivdvorkovoy <Y6cA4V9maGAVeyQ>
2022-05-15 11:46:28 +0300

    Добавлен класс Request

--- app/Http/Controllers/Post/StoreController.php
+++ app/Http/Controllers/Post/StoreController.php
@@ -3,27 +3,19 @@
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\StoreRequest;
 use App\Models\Post;
 
 class StoreController extends Controller
 {
-    public function __invoke()
+    public function __invoke(StoreRequest $request)
     {
-        $data = request()->validate([
-            'title' => 'string',
-            'content' => 'string',
-            'image' => 'string',
-            'likes' => 'integer',
-            'category_id' => 'integer',
-            'tags' => ''
-
-        ]);
+        $data = $request->validated();
 
         $tags = $data['tags'];
         unset($data['tags']);
 
         $post = Post::create($data);
-
         $post->tags()->attach($tags);
 
         return redirect()->route('post.index');
--- app/Http/Controllers/Post/UpdateController.php
+++ app/Http/Controllers/Post/UpdateController.php
@@ -3,27 +3,20 @@
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\UpdateRequest;
 use App\Models\Post;
 
 class UpdateController extends Controller
 {
-    public function __invoke(Post $post)
+    public function __invoke(UpdateRequest $request, Post $post)
     {
-        $data = request()->validate([
-            'title' => 'string',
-            'content' => 'string',
-            'image' => 'string',
-            'likes' => 'integer',
-            'category_id' => 'integer',
-            'tags' => ''
-        ]);
+        $data = $request->validated();
 
         $tags = $data['tags'];
         unset($data['tags']);
 
         $post->update($data);
         $post = $post->fresh();
-
         $post->tags()->sync($tags);
 
         return redirect()->route('post.show', $post->id);
